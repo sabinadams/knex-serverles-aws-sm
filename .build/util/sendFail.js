94e69e40bc5b6a6225324fb767ef8b70d58f0bb8
Sabin Adams <sabintheworld@gmail.com>
2023-03-28 10:44:52 -0700

    Removes unneeded folder

--- .build/util/sendFail.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (message) => {
-    return {
-        statusCode: 400,
-        body: JSON.stringify({ message }),
-    };
-};
